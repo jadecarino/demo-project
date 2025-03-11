e4f08c9680cd94db2254919a16b50f6b04e5914e
Jade Carino <carino_jade@yahoo.co.uk>
2025-03-11 10:30:22 +0000

    Add another test for batch
    Signed-off-by: Jade Carino <carino_jade@yahoo.co.uk>

--- dev.galasa.example.simbank/dev.galasa.example.simbank.login/src/main/java/dev/galasa/example/simbank/login/TestLogin.java
+++ dev.galasa.example.simbank/dev.galasa.example.simbank.login/src/main/java/dev/galasa/example/simbank/login/TestLogin.java
@@ -1,60 +1,56 @@
 package dev.galasa.example.simbank.login;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
-import dev.galasa.ICredentialsUsernamePassword;
 import dev.galasa.Test;
-import dev.galasa.core.manager.CoreManager;
-import dev.galasa.core.manager.ICoreManager;
 import dev.galasa.zos.IZosImage;
 import dev.galasa.zos.ZosImage;
 import dev.galasa.zos3270.ITerminal;
 import dev.galasa.zos3270.Zos3270Terminal;
+import dev.galasa.http.HttpClient;
+import dev.galasa.http.IHttpClient;
+import dev.galasa.core.manager.*;
 
 @Test
 public class TestLogin {
 
-    @ZosImage(imageTag = "SIMBANK")
-    public IZosImage image;
+	@ZosImage(imageTag = "SIMBANK")
+	public IZosImage image;
 
-    @Zos3270Terminal(imageTag = "SIMBANK")
-    public ITerminal terminal;
+	@Zos3270Terminal(imageTag = "SIMBANK")
+	public ITerminal terminal;
 
-    @CoreManager
-    public ICoreManager coreManager;
+	@HttpClient
+	public IHttpClient client;
 
-    private static final String CREDENTIALS_ID = "MYSIMBANKUSER";
+	@CoreManager
+	public ICoreManager coreManager;
 
-    @Test
-    public void checkBankIsAvailable() throws Exception {
-        // Get the credentials that will be used to log in to SimBank
-        ICredentialsUsernamePassword credentials = (ICredentialsUsernamePassword) coreManager.getCredentials(CREDENTIALS_ID);
+	@Test
+	public void checkBankIsAvailable() throws Exception {
 
-        // Register the password to the confidential text filtering service
-        String password = credentials.getPassword();
-        coreManager.registerConfidentialText(password, "The password to access SimBank");
+		// Register password as confidential text so it's not shown in any run logs.
+		coreManager.registerConfidentialText("SYS1", "IBMUSER password");
 
-        // Logon through the session manager
-        terminal.waitForKeyboard()
-            .positionCursorToFieldContaining("Userid").tab()
-            .type(credentials.getUsername())
-            .positionCursorToFieldContaining("Password").tab()
-            .type(password)
-            .enter()
-            .waitForKeyboard();
+		// Enter user ID and password with 3270 terminal.
+		terminal.waitForKeyboard()
+			.positionCursorToFieldContaining("Userid").tab().type("IBMUSER")
+			.positionCursorToFieldContaining("Password").tab().type("SYS1")
+			.enter().waitForKeyboard();
 
-        // Assert that the session manager has a bank session available
-        assertThat(terminal.retrieveScreen()).containsOnlyOnce("SIMPLATFORM MAIN MENU");
-        assertThat(terminal.retrieveScreen()).containsOnlyOnce("BANKTEST");
+		// Check that the screen after login is displayed on the screen.
+		assertThat(terminal.retrieveScreen()).containsOnlyOnce("SIMPLATFORM MAIN MENU");
+		assertThat(terminal.retrieveScreen()).containsOnlyOnce("BANKTEST");
 
-        // Open banking application
-        terminal.pf1().waitForKeyboard().clear().waitForKeyboard();
+		terminal.pf1().waitForKeyboard().clear().waitForKeyboard();
+		
+		// Open the banking application.
+		terminal.type("bank").enter().waitForKeyboard();
 
-        terminal.type("bank").enter().waitForKeyboard();
-
-        // Assert that the bank menu is showing
-        assertThat(terminal.retrieveScreen()).containsOnlyOnce("Options     Description        PFKey ");
-        assertThat(terminal.retrieveScreen()).containsOnlyOnce("BROWSE      Browse Accounts    PF1");
-        assertThat(terminal.retrieveScreen()).containsOnlyOnce("TRANSF      Transfer Money     PF4");
-    }
+		// Check that the banking application menu is displayed on the screen.
+		assertThat(terminal.retrieveScreen()).containsOnlyOnce("Options     Description        PFKey ");
+		assertThat(terminal.retrieveScreen()).containsOnlyOnce("BROWSE      Browse Accounts    PF1");
+		assertThat(terminal.retrieveScreen()).containsOnlyOnce("TRANSF      Transfer Money     PF4");
+		
+	}
 }
---
+++ dev.galasa.example.simbank/dev.galasa.example.simbank.openaccount/src/main/java/dev/galasa/example/simbank/openaccount/TestOpenAccount.java
@@ -0,0 +1,97 @@
+package dev.galasa.example.simbank.openaccount;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.assertj.core.api.Fail;
+
+import dev.galasa.Test;
+import dev.galasa.artifact.BundleResources;
+import dev.galasa.artifact.IBundleResources;
+import dev.galasa.core.manager.Logger;
+import dev.galasa.zos.IZosImage;
+import dev.galasa.zos.ZosImage;
+import dev.galasa.zosbatch.IZosBatch;
+import dev.galasa.zosbatch.IZosBatchJob;
+import dev.galasa.zosbatch.IZosBatchJobname;
+import dev.galasa.zosbatch.ZosBatch;
+import dev.galasa.zosbatch.ZosBatchJobname;
+
+@Test
+public class TestOpenAccount {
+
+	@ZosImage(imageTag = "SIMBANK")
+	public IZosImage image;
+	
+	@ZosBatch(imageTag = "SIMBANK")
+	public IZosBatch zosBatch;
+	
+	@ZosBatchJobname(imageTag = "SIMBANK")
+	public IZosBatchJobname zosBatchJobname;
+
+	@BundleResources
+	public IBundleResources resources;
+	
+	@Logger
+	public Log logger;
+
+	/**
+	 * Test which uses the SIMBANK batch job to open a number of new accounts.
+	 * The test passes if the job completes successfully (RC=0000)
+	 * 
+	 * This test shows why accounts should not be hard coded.
+	 * The initial test run will pass as the hard coded account numbers are unique,
+	 * however subsequent runs will fail as these account numbers are being stored while
+	 * the Simbank application is running. The accounts cannot be added and a failed return code (RC=0012)
+	 * is received resulting in a failed test.
+	 * 
+	 * Without changing the test code, the only way to make the test pass again would be to restart Simbank before running the test.
+	 * 
+	 * To avoid this, accounts should not be hard coded.
+	 * Account numbers could be randomly generated with each one passing a check if it already exists before being submitted as a batch job.
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testBatchOpenAccount() throws Exception {
+		// Create a list of accounts to create
+		List<String> accountList = new LinkedList<>();
+		accountList.add("901000001,20-40-60,1000");
+		accountList.add("901000002,20-40-60,1000");
+		accountList.add("901000003,20-40-60,1000");
+		accountList.add("901000004,20-40-60,1000");
+		accountList.add("901000005,20-40-60,1000");
+		accountList.add("901000006,20-40-60,1000");
+		accountList.add("901000007,20-40-60,1000");
+		accountList.add("901000008,20-40-60,1000");
+		accountList.add("901000009,20-40-60,1000");
+
+		// Create the substitution parameters for the JCL
+		HashMap<String, Object> parameters = new HashMap<>();
+		parameters.put("CONTROL", "ACCOUNT_OPEN");
+		parameters.put("DATAIN", String.join("\n", accountList));
+		
+		// Load the JCL with the given substitution parameters
+		String jcl = resources.retrieveSkeletonFileAsString("/skeletons/SIMBANK.jcl", parameters);
+		
+		// Submit the JCL
+		IZosBatchJob batchJob = zosBatch.submitJob(jcl, zosBatchJobname);
+		
+		// Wait for the batch job to complete
+		logger.info("batchJob.toString() = " +  batchJob.toString());
+		int rc = batchJob.waitForJob();
+		
+		// If highest CC was not 0, fail the test
+		if (rc != 0) {
+			// Print the job output to the run log
+			batchJob.retrieveOutput().forEach(jobOutput ->
+				logger.info("batchJob.retrieveOutput(): " + jobOutput.getDdname() + "\n" + jobOutput.getRecords() + "\n")
+			);
+			Fail.fail("Batch job failed RETCODE=" + batchJob.getRetcode() + " Check batch job output");
+			
+		}
+		logger.info("Batch job complete RETCODE=" + batchJob.getRetcode());
+	}
+}
